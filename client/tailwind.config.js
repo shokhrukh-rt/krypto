0740ce12cc62b31facfc2e3fc73c09d6979d4996
shokhrukh-rt <mr.shohruh.1003@gmail.com>
2022-03-04 02:34:30 +0900

    Initial commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	content: [
+		"./src/**/*.{html,jsx,js, css}",
+		"./pages/**/*.{html,jsx,js}",
+		"./components/**/*.{html,jsx,js}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
